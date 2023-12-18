5ac07da0cf6152d3f7077ca75fd0a466b11dee59
Jason Swift <anemele@outlook.com>
2023-12-18 21:02:39 +0800

    update

--- disk_usage/__main__.py
+++ disk_usage/__main__.py
@@ -1,43 +1,10 @@
 """ Disk usage? I don't know."""
 import argparse
 import os
-from typing import Iterable
 
-from rich import print
+from .core import count, pprint
 
-
-def count_root(root: tuple[str, list[str], list[str]]) -> tuple[int, int, int]:
-    """return a tuple of
-    (number of dirs, number of files, size of files)"""
-    r, d, f = root
-    pth = (os.path.join(r, i) for i in f)
-    return len(d), len(f), sum(map(os.path.getsize, pth))
-
-
-def count(directory: str, recursive: bool) -> Iterable[tuple[int, int, int]]:
-    walker = os.walk(directory)
-    yield count_root(next(walker))
-
-    if recursive:
-        try:
-            for item in map(count_root, walker):
-                yield item
-        except KeyboardInterrupt:
-            pass
-
-
-def pprint(root: str, count: Iterable[tuple[int, int, int]]):
-    print(' ', os.path.abspath(root), '\n')
-    nd, nf, ns = 0, 0, 0
-    for d, f, s in count:
-        nd += d
-        nf += f
-        ns += s
-        print(f'  {nd} dir(s)  {nf} file(s)  {ns:,} bytes', end='\r')
-    print()
-
-
-parser = argparse.ArgumentParser(description=__doc__)
+parser = argparse.ArgumentParser(prog=__package__, description=__doc__)
 parser.add_argument(
     'dir',
     type=str,
